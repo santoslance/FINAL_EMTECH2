ac61ad34e4d294f6e8069063a07006c024a88f26
geb <156665205+santoslance@users.noreply.github.com>
2024-12-12 19:41:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def import_and_predict(image_data, model):
     size = (128, 128)  # Adjust to match your model's input size
 
     # Resize the image
-    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    image = ImageOps.fit(image_data, size, Image.Resampling.LANCZOS)
     img = np.asarray(image)
     img = cv2.resize(img, size, interpolation=cv2.INTER_NEAREST)
 
